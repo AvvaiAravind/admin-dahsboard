1eb53e12261af378337758e2a87c0ab384cada75
Avvai Aravind <avvaiaravind@gmail.com>
2024-10-02 17:44:14 +0530

    finished toggling side bar

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-const home = document.querySelector(".home");
-home.addEventListener("click", () => {});
+const mobileIcon = document.querySelector(".mobile-dashboard");
+mobileIcon.addEventListener("click", () => {
+  if (window.innerWidth < 768) {
+    console.log("logged");
+    const dashboardHeading = document.querySelector(".dashboard-heading");
+    const dashboardNav = document.querySelector("nav");
+    dashboardHeading.classList.toggle("display-unset");
+    dashboardNav.classList.toggle("display-unset");
+  }
+});
